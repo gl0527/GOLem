28805bd20c71fb393d7973e931fd41c58a3f02f2
gl0527 <gurzolajos27@gmail.com>
2022-12-06 21:33:17 +0100

    Put error messages to stderr

--- gol.cpp
+++ gol.cpp
@@ -5,41 +5,41 @@
 int main(int argc, char **argv)
 {
     if (argc != 2) {
-        printf("Wrong number of arguments provided! 1 argument is needed for the file path.\n");
+        fprintf(stderr, "Wrong number of arguments provided! 1 argument is needed for the file path.\n");
         return 1;
     }
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("Error @ SDL2 initialization: %s.\n", SDL_GetError());
+        fprintf(stderr, "Error @ SDL2 initialization: %s.\n", SDL_GetError());
         return 1;
     }
 
     if (0 == IMG_Init(IMG_INIT_JPG)) {
-        printf("Error @ SDL2_Image initialization: %s.\n", IMG_GetError());
+        fprintf(stderr, "Error @ SDL2_Image initialization: %s.\n", IMG_GetError());
         return 1;
     }
 
     SDL_Window *window = SDL_CreateWindow("Empty Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
     if (nullptr == window) {
-        printf("Error @ window creation: %s.\n", SDL_GetError());
+        fprintf(stderr, "Error @ window creation: %s.\n", SDL_GetError());
         return 1;
     }
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
     if (nullptr == renderer) {
-        printf("Error @ renderer creation: %s.\n", SDL_GetError());
+        fprintf(stderr, "Error @ renderer creation: %s.\n", SDL_GetError());
         return 1;
     }
 
     SDL_Surface *image = IMG_Load(argv[1]);
     if (nullptr == image) {
-        printf("Error @ loading image: %s.\n", IMG_GetError());
+        fprintf(stderr, "Error @ loading image: %s.\n", IMG_GetError());
         return 1;
     }
 
     SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, image);
     if (nullptr == texture) {
-        printf("Error @ creating texture: %s.\n", SDL_GetError());
+        fprintf(stderr, "Error @ creating texture: %s.\n", SDL_GetError());
         return 1;
     }
 
